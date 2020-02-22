178e8da12d9181e58a6983dadec386a94dce11c4
Camilo Guio <camiloguio@gmail.com>
2020-02-21 19:41:51 -0500

    Haciendo el servicio

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HijoComponent } from '../components/hijo/hijo.component';
+import { ComponentsModule } from '../components/components.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ComponentsModule
+  ]
+})
+export class PagesModule { }
